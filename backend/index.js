05e2ccac13094a0cba869f5875476fc846c572c5
Tushar Dilip Padiye <tushar07padiye@gmail.com>
2023-07-13 20:42:03 +0530

    f45

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,14 @@ const app = express()
 const port = 5000
 const connector = require("./db")
 
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept , Authorization"
+  );
+  next();
+})
 
 
 app.get('/', (req, res) => {
@@ -17,11 +25,3 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept , Authorization"
-    );
-    next();
-  })
